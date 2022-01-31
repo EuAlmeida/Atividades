30246b15674ea557684b44ee6c6538f0b50a547a
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-01-31 14:10:16 -0300

    alo

---
+++ FirebaseEhtml/js-firebase/JS/init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBn2Nz_aKGVe2UzNerEYPZn_AOIAKXs6LI",
+    authDomain: "lista-de-tarefas-7aa42.firebaseapp.com",
+    projectId: "lista-de-tarefas-7aa42",
+    storageBucket: "lista-de-tarefas-7aa42.appspot.com",
+    messagingSenderId: "903541011867",
+    appId: "1:903541011867:web:e66177543d08c982519d3e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
